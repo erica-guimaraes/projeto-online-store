ef2255632700ceafd95a5756223433168419a2b1
Vitor Gomes <vitorgomes14@hotmail.com>
2023-04-19 19:30:41 -0300

    Progresso requisito 5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import CategoryList from '../components/CategoryList';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends Component {
+  state = {
+    nome: '',
+    category: '',
+    productsList: [],
+  };
+
+  handleClickBusca = async () => {
+    const { nome, category } = this.state;
+    const result = await getProductsFromCategoryAndQuery(category, nome);
+    this.setState({
+      productsList: result.results,
+    });
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      nome: value,
+    });
+  };
+
   render() {
+    const { productsList } = this.state;
     return (
       <div>
         <p
@@ -11,6 +33,17 @@ class Home extends Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <input
+          data-testid="query-input"
+          type="text"
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="query-button"
+          onClick={ this.handleClickBusca }
+        >
+          Buscar
+        </button>
         <CategoryList />
         <Link
           to="/carrinho"
@@ -18,6 +51,13 @@ class Home extends Component {
         >
           <button>carrinho</button>
         </Link>
+        {productsList.length === 0
+          ? (<p>Nenhum produto foi encontrado</p>)
+          : (productsList.map((product) => (
+            <div key={ product.id }>
+              <p />
+            </div>
+          )))}
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,13 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const resAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/${query}/${categoryId}`);
+  const resAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const resultJSON = await resAPI.json();
+  return resultJSON;
+}
+
+export async function getProductsByQuery(query) {
+  const resAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
   const resultJSON = await resAPI.json();
   return resultJSON;
 }
